3b833a1fd80e56e4ec82139bac190c05ed1ace26
RomainJlt <romainjollivet0@gmail.com>
2024-03-25 09:03:47 +0100

    stats personnage

---
+++ aventurier.ts
@@ -0,0 +1,10 @@
+import Character from "./class.ts";
+
+
+const guerrier = new Character("Guerrier", 90, 90, 100, 100);
+const mage = new Character("Mage", 90, 90, 100, 100);
+const paladin = new Character("Paladin", 90, 90, 100, 100);
+const barbare = new Character("Barbare", 90, 90, 100, 100);
+const prêtre = new Character("Prêtre", 90, 90, 100, 100);
+const voleur = new Character("Voleur", 90, 90, 100, 100);
+const bucheron = new Character("Bucheron", 90, 90, 100, 100);
