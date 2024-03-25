45516bc04b5cfb3096089fc19655891eee16cf2f
anajmeld <adamabkar.najmeldeen@ynov.com>
2024-03-25 07:58:45 +0100

    class character

---
+++ class.ts
@@ -0,0 +1,16 @@
+class Character {
+    String name;
+    int healthPoints;
+    int physicalAttack;
+    int physicalDefense;
+    int speed;
+
+    // Constructor
+    public Character(String name, int healthPoints, int physicalAttack, int physicalDefense, int speed) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.speed = speed;
+    }
+}
