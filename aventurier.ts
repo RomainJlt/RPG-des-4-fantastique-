c6852966ebea0c56d38dd5d288342b5ac2d7e46e
RomainJlt <romainjollivet0@gmail.com>
2024-03-25 10:23:46 +0100

    class perso perso

--- aventurier.ts
+++ aventurier.ts
@@ -1,10 +1,21 @@
 import Character from "./class.ts";
 
+// Name, physicalAttack, physicalDefense ,speed, HPMax, HPCurrent, attackPotency, canBeHurt, canBeCured, canBeResurrected 
+const guerrier =    new Character("Théophile Le Luisant",               40, 70, 50, 100, 100, 80, false, false, false);
+const mage =        new Character("Merlin Gros Baton",                  10, 10, 50, 100, 100, 80, false, false, false);
+const paladin =     new Character("Dartagnan Durex",                    30, 75, 60, 100, 100, 80, false, false, false);
+const barbare =     new Character("Marcel Le Pénètrepeau",              30, 10, 30, 100, 100, 80, false, false, false);
+const prêtre =      new Character("Jean De Pédolandie",                 20, 10, 100, 100, 100, 80, false, false, false);
+const voleur =      new Character("Marcus Le Voleur D'Innocence",       50, 50, 80, 100, 100, 80, false, false, false);
 
-const guerrier = new Character("Guerrier", 90, 90, 100, 100);
-const mage = new Character("Mage", 90, 90, 100, 100);
-const paladin = new Character("Paladin", 90, 90, 100, 100);
-const barbare = new Character("Barbare", 90, 90, 100, 100);
-const prêtre = new Character("Prêtre", 90, 90, 100, 100);
-const voleur = new Character("Voleur", 90, 90, 100, 100);
-const bucheron = new Character("Bucheron", 90, 90, 100, 100);
+
+
+
+
+const bucheron =    new Character("Paul La Grosse Poutre",              100, 100, 100, 100, 100, 80, false, false, false);
+const scammer =     new Character("Axel Le Scammer De Mémés Baveuses",  100, 100, 100, 100, 100, 80, false, false, false);
+const prince =      new Character("Le Prince Des ténèbres",             100, 100, 100, 100, 100, 80, false, false, false);
+const druide =      new Character("Najm L'Anaconda",                    100, 100, 100, 100, 100, 80, false, false, false);
+const Dieu =        new Character("Julien La Divinité",                 1000, 1000, 1000, 1000, 1000, 100, false, false, false);
+
+
