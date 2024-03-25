53e8b68eedeed1b86d637739ef52f3b30b7f98ef
Paul Decatoire <decatopaul@gmail.com>
2024-03-25 08:57:08 +0100

    Ajout de class.ts avec le constructeur

--- class.ts
+++ class.ts
@@ -1,13 +1,32 @@
-class Character{
+export default class Character{
     name: string;
     physicalAttack: number;
     physicalDefense: number;
     speed: number;
     HPMax: number;
     HPCurrent: number;
+    attackPotency: number;
     canBeHurt: boolean;
     canBeCured: boolean;
     canBeResurrected: boolean;
+
+
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, HPMax : number, HPCurrent : number, attackPotency : number, canBeHurt : boolean, canBeCured : boolean, canBeResurrected: boolean){
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.speed = speed;
+        this.HPMax = HPMax;
+        this.HPMax = HPMax;
+        this.HPCurrent = HPCurrent;
+        this.canBeHurt = canBeHurt;
+        this.canBeCured = canBeCured;
+        this.canBeResurrected = canBeResurrected;
+    }
+
+
+
+
 }
 
 class Fight{
