98b0767be5589ffeeb1c23ffe32802ac53b14f89
CryptoDebug <axel10.lemaitre@gmail.com>
2024-03-25 11:12:28 +0100

    Created the menu + ansi + comments

---
+++ class.ts
@@ -0,0 +1,53 @@
+export default class Character{
+    name: string;
+    physicalAttack: number;
+    physicalDefense: number;
+    speed: number;
+    HPMax: number;
+    HPCurrent: number;
+    canBeHurt: boolean;
+    canBeCured: boolean;
+    canBeResurrected: boolean;
+
+
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, HPMax : number){
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.speed = speed;
+        this.HPMax = HPMax;
+        this.HPMax = HPMax;
+        this.HPCurrent = HPCurrent;
+        this.canBeHurt = canBeHurt;
+        this.canBeCured = canBeCured;
+        this.canBeResurrected = canBeResurrected;
+
+
+        // Animal.populationSize++;
+    }
+
+
+
+
+}
+
+class Fight{
+    protagonist: Character;
+    antagonist: Character;
+}
+
+class GameManager{
+
+}
+
+export class Menu{
+    move: string;
+    openAChest: boolean;
+    leave: boolean;
+
+    constructor(move : string, openAChest : boolean, leave : boolean){
+        this.move = move;
+        this.openAChest = openAChest;
+        this.leave = leave;
+    }
+}
