23fb841d4476054fea308eedd215155cf91cff20
RomainJlt <romainjollivet0@gmail.com>
2024-03-27 09:06:47 +0100

    boss

--- class.ts
+++ class.ts
@@ -1,4 +1,5 @@
-export default class Character{
+export class Character {
+
     name: string;
     physicalAttack: number;
     physicalDefense: number;
@@ -10,8 +11,21 @@ export default class Character{
     canBeCured: boolean;
     canBeResurrected: boolean;
 
+    /**
+     * 
+     * @param name The name of the aventurier
+     * @param physicalAttack  The physical attack of the aventurier
+     * @param physicalDefense  The physical defense of the aventurier
+     * @param speed  The speed attack of the aventurier
+     * @param HPMax  The max HP of the aventurier
+     * @param HPCurrent  The current HP of the aventurier
+     * @param attackPotency  The potential for a successful attack
+     * @param canBeHurt 
+     * @param canBeCured 
+     * @param canBeResurrected 
+     */
+    constructor(name: string, physicalAttack: number, physicalDefense: number, speed: number, HPMax: number, HPCurrent: number, attackPotency: number, canBeHurt: boolean, canBeCured: boolean, canBeResurrected: boolean) {
 
-    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, HPMax : number, HPCurrent : number, attackPotency : number, canBeHurt : boolean, canBeCured : boolean, canBeResurrected: boolean){
         this.name = name;
         this.physicalAttack = physicalAttack;
         this.physicalDefense = physicalDefense;
@@ -23,10 +37,37 @@ export default class Character{
         this.canBeCured = canBeCured;
         this.canBeResurrected = canBeResurrected;
     }
+}
+
+export class Monster {
+    name: string;
+    physicalAttack: number;
+    physicalDefense: number;
+    speed: number;
+    HPMax: number;
+    HPCurrent: number;
+    attackPotency: number;
 
 
-
-
+/**
+ * 
+ * @param name The name of the monster
+ * @param physicalAttack The physical attack of the monster
+ * @param physicalDefense The physical defense of the monster
+ * @param speed The speed attack of the monster
+ * @param HPMax The max HP of the monster
+ * @param HPCurrent The current HP of the monster
+ * @param attackPotency The potential for a successful attack
+ */
+    constructor(name: string, physicalAttack: number, physicalDefense: number, speed: number, HPMax: number, HPCurrent: number, attackPotency: number) {
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.speed = speed;
+        this.HPMax = HPMax;
+        this.HPMax = HPMax;
+        this.HPCurrent = HPCurrent;
+    }
 }
 
 class Fight{
