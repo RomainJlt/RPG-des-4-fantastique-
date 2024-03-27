315c39ed85190080ed007c08c2b08772120abfed
Paul Decatoire <decatopaul@gmail.com>
2024-03-27 09:40:58 +0100

    Ajout de class

--- class.ts
+++ class.ts
@@ -2,6 +2,8 @@ export default class Character{
     name: string;
     physicalAttack: number;
     physicalDefense: number;
+    magicalAttack: number;
+    magicalDefense: number;
     speed: number;
     HPMax: number;
     HPCurrent: number;
@@ -11,14 +13,17 @@ export default class Character{
     canBeResurrected: boolean;
 
 
-    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, HPMax : number, HPCurrent : number, attackPotency : number, canBeHurt : boolean, canBeCured : boolean, canBeResurrected: boolean){
+    constructor(name : string, physicalAttack : number, physicalDefense : number, magicalAttack : number, magicalDefense : number, speed : number, HPMax : number, HPCurrent : number, attackPotency : number, canBeHurt : boolean, canBeCured : boolean, canBeResurrected: boolean){
         this.name = name;
         this.physicalAttack = physicalAttack;
         this.physicalDefense = physicalDefense;
+        this.magicalAttack = magicalAttack;
+        this.magicalDefense = magicalDefense;
         this.speed = speed;
         this.HPMax = HPMax;
         this.HPMax = HPMax;
         this.HPCurrent = HPCurrent;
+        this.attackPotency = attackPotency;
         this.canBeHurt = canBeHurt;
         this.canBeCured = canBeCured;
         this.canBeResurrected = canBeResurrected;
