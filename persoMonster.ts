314788588ba97e3195f17d5b8fb4014895679714
RomainJlt <romainjollivet0@gmail.com>
2024-04-08 10:18:35 +0200

    choose group

--- persoMonster.ts
+++ persoMonster.ts
@@ -1,4 +1,4 @@
-import { Character, Monster } from "./class";
+import { Character, Monster } from "./class.ts";
 
 
 // Name, physicalAttack, physicalDefense ,speed, HPMax, HPCurrent, attackPotency, canBeHurt, canBeCured, canBeResurrected 
@@ -16,7 +16,7 @@ export const Geek =        new Character("Romain Ultimement Nul",              1
 export const Druide =      new Character("Najm L'Anaconda",                    100, 100, 100, 100, 100, 80, false, false, false);
 export const Dieu =        new Character("Julien La Divinité",                 1000, 1000, 1000, 1000, 1000, 100, false, false, false);
 
-export const humain : Character[] = [Guerrier, Mage, Paladin, Barbare, Prêtre, Voleur, Bucheron, Scammer, Geek, Druide, Dieu];
+// export const humain : Character[] = [Guerrier, Mage, Paladin, Barbare, Prêtre, Voleur, Bucheron, Scammer, Geek, Druide, Dieu];
 
 
 
@@ -29,4 +29,4 @@ const Dragon =      new Monster("DG",       10, 10, 10, 50, 50, 5);
 
 export const monster : Monster[] = [Zombie, Vampire, LoupGarou, Dragon];
 
-export { Character };
+// export { Character };
