2b62a228a6c149ded481b0ea37a16910d50204f3
Paul Decatoire <decatopaul@gmail.com>
2024-04-08 08:32:25 +0200

    Ajout de persoMonster modifié

--- persoMonster.ts
+++ persoMonster.ts
@@ -1,10 +1,10 @@
-import { Character, Monster } from "./class";
+import { Character, Monster } from "./class.ts";
 
 
 // Name, physicalAttack, physicalDefense ,speed, HPMax, HPCurrent, attackPotency, canBeHurt, canBeCured, canBeResurrected 
-const guerrier =    new Character("Théophile Le Luisant",               40, 70, 50, 100, 100, 80, false, false, false);
-const mage =        new Character("Merlin Gros Baton",                  10, 10, 50, 100, 100, 80, false, false, false);
-const paladin =     new Character("Dartagnan Durex",                    30, 75, 60, 100, 100, 80, false, false, false);
+const guerrier =    new Character("Théophile Le Luisant",               40, 70, 0, 0, 50, 100, 100, 80, false, false, false);
+const mage =        new Character("Merlin Gros Baton",                  10, 10, 50, 30, 50, 100, 100, 80, false, false, false);
+const paladin =     new Character("Dartagnan Durex",                    30, 75, 20, 50, 60, 100, 100, 80, false, false, false);
 const barbare =     new Character("Marcel Le Pénètrepeau",              30, 10, 30, 100, 100, 80, false, false, false);
 const prêtre =      new Character("Jean De Pédolandie",                 20, 10, 100, 100, 100, 80, false, false, false);
 const voleur =      new Character("Marcus Le Voleur D'Innocence",       50, 50, 80, 100, 100, 80, false, false, false);
@@ -28,3 +28,5 @@ const loupGarou = new Monster("Charly", 10, 10, 10, 50, 50, 5);
 const dragon = new Monster("DG", 10, 10, 10, 50, 50, 5);
 
 export const monster : Monster[] = [zombie, vampire, loupGarou,dragon];
+
+export {Character};
