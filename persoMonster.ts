603e6c5170d8abc81cc1e1e9c337422562268306
RomainJlt <romainjollivet0@gmail.com>
2024-04-09 11:03:14 +0200

    chooseG mai

--- persoMonster.ts
+++ persoMonster.ts
@@ -1,13 +1,13 @@
 import { Character, Monster } from "./class.ts";
 
 
-// Name, physicalAttack, physicalDefense ,speed, HPMax, HPCurrent, attackPotency, canBeHurt, canBeCured, canBeResurrected 
-const guerrier =    new Character("Théophile Le Luisant",               40, 70, 0, 0, 50, 100, 100, 80, false, false, false);
-const mage =        new Character("Merlin Gros Baton",                  10, 10, 50, 30, 50, 100, 100, 80, false, false, false);
-const paladin =     new Character("Dartagnan Durex",                    30, 75, 20, 50, 60, 100, 100, 80, false, false, false);
-const barbare =     new Character("Marcel Le Pénètrepeau",              30, 10, 30, 100, 100, 80, false, false, false);
-const prêtre =      new Character("Jean De Pédolandie",                 20, 10, 100, 100, 100, 80, false, false, false);
-const voleur =      new Character("Marcus Le Voleur D'Innocence",       50, 50, 80, 100, 100, 80, false, false, false);
+// Name, physicalAttack, physicalDefense, magicalAttack, magicalDefense, speed, HPMax, HPCurrent, attackPotency, canBeHurt, canBeCured, canBeResurrected 
+export const Guerrier =    new Character("Théophile Le Luisant",               40, 70, 0, 0, 50, 100, 100, 80, false, false, false);
+export const Mage =        new Character("Merlin Gros Baton",                  10, 10, 50, 30, 50, 100, 100, 80, false, false, false);
+export const Paladin =     new Character("Dartagnan Durex",                    30, 75, 20, 50, 60, 100, 100, 80, false, false, false);
+export const Barbare =     new Character("Marcel Le Pénètrepeau",              30, 10, 0, 0, 30, 100, 100, 80, false, false, false);
+export const Prêtre =      new Character("Jean De Pédolandie",                 20, 10, 100, 100, 100, 80, false, false, false);
+export const Voleur =      new Character("Marcus Le Voleur D'Innocence",       50, 50, 80, 100, 100, 80, false, false, false);
 
 
 export const Bucheron =    new Character("Paul La Grosse Poutre",              100, 100, 100, 100, 100, 80, false, false, false);
@@ -16,7 +16,7 @@ export const Geek =        new Character("Romain Ultimement Nul",              1
 export const Druide =      new Character("Najm L'Anaconda",                    100, 100, 100, 100, 100, 80, false, false, false);
 export const Dieu =        new Character("Julien La Divinité",                 1000, 1000, 1000, 1000, 1000, 100, false, false, false);
 
-// export const humain : Character[] = [Guerrier, Mage, Paladin, Barbare, Prêtre, Voleur, Bucheron, Scammer, Geek, Druide, Dieu];
+export const humain : Character[] = [Guerrier, Mage, Paladin, Barbare, Prêtre, Voleur, Bucheron, Scammer, Geek, Druide, Dieu];
 
 
 
@@ -27,6 +27,6 @@ const Vampire =     new Monster("Florian",  10, 10, 10, 50, 50, 5);
 const LoupGarou =   new Monster("Charly",   10, 10, 10, 50, 50, 5);
 const Dragon =      new Monster("DG",       10, 10, 10, 50, 50, 5);
 
-export const monster : Monster[] = [zombie, vampire, loupGarou,dragon];
+export const monster : Monster[] = [Zombie, Vampire, LoupGarou, Dragon];
 
 export {Character};
