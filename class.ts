ba9b067b65f505b1f6673d6d634cebb897badf5c
RomainJlt <romainjollivet0@gmail.com>
2024-04-12 11:18:19 +0200

    push class/persoMonster/readme

--- class.ts
+++ class.ts
@@ -1,41 +1,84 @@
-export default class Character{
+export class Character {
+
+    classAdventur: string;
     name: string;
     physicalAttack: number;
     physicalDefense: number;
+    magicalAttack: number;
+    magicalDefense: number;
+    mana: number;
     speed: number;
     HPMax: number;
     HPCurrent: number;
+    attackPotency: number;
     canBeHurt: boolean;
     canBeCured: boolean;
     canBeResurrected: boolean;
+    isKO: boolean;
 
+    /**
+     * 
+     * @param name The name of the aventurier
+     * @param physicalAttack  The physical attack of the aventurier
+     * @param physicalDefense  The physical defense of the aventurier
+     * @param speed  The speed attack of the aventurier
+     * @param HPMax  The max HP of the aventurier
+     * @param HPCurrent  The current HP of the aventurier
+     * @param attackPotency  The potential for a successful attack
+     * @param canBeHurt 
+     * @param canBeCured 
+     * @param canBeResurrected 
+     */
+    constructor(classAdventur: string, name: string, physicalAttack: number, physicalDefense: number, magicalAttack: number, magicalDefense: number, mana: number, speed: number, HPMax: number, HPCurrent: number, attackPotency: number, canBeHurt: boolean, canBeCured: boolean, canBeResurrected: boolean, isKO: boolean) {
 
-    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, HPMax : number){
+        this.classAdventur = classAdventur;
         this.name = name;
         this.physicalAttack = physicalAttack;
         this.physicalDefense = physicalDefense;
+        this.magicalAttack = magicalAttack;
+        this.magicalDefense = magicalDefense;
+        this.mana = mana;
         this.speed = speed;
         this.HPMax = HPMax;
-        this.HPMax = HPMax;
         this.HPCurrent = HPCurrent;
+        this.attackPotency = attackPotency;
         this.canBeHurt = canBeHurt;
         this.canBeCured = canBeCured;
         this.canBeResurrected = canBeResurrected;
-
-
-        // Animal.populationSize++;
+        this.isKO = isKO;
     }
-
-
-
-
 }
 
-class Fight{
-    protagonist: Character;
-    antagonist: Character;
-}
+export class Monster {
+    name: string;
+    physicalAttack: number;
+    physicalDefense: number;
+    magicalDefense: number;
+    speed: number;
+    HPMax: number;
+    HPCurrent: number;
+    attackPotency: number;
 
-class GameManager{
 
+/**
+ * 
+ * @param name The name of the monster
+ * @param physicalAttack The physical attack of the monster
+ * @param physicalDefense The physical defense of the monster
+ * @param speed The speed attack of the monster
+ * @param HPMax The max HP of the monster
+ * @param HPCurrent The current HP of the monster
+ * @param attackPotency The potential for a successful attack
+ */
+    constructor(name: string, physicalAttack: number, physicalDefense: number, magicalDefense: number, speed: number, HPMax: number, HPCurrent: number, attackPotency: number) {
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.magicalDefense = magicalDefense;
+        this.speed = speed;
+        this.HPMax = HPMax;
+        
+        this.HPCurrent = HPCurrent;
+        this.attackPotency = attackPotency;
+    }
 }
