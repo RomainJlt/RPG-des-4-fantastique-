63b73d9480696fdebdeca78ac6bff8744527532e
Paul Decatoire <decatopaul@gmail.com>
2024-04-13 13:01:38 +0200

    Ajout d'une map

--- process.ts
+++ process.ts
@@ -4,9 +4,19 @@ import { adventurers } from "./chooseGroup.ts";
 import { fightBoss } from "./history.ts";
 
 export function processing(){
+    let l = 1;
+    let object = ["Potion", "Morceau d'étoile", "Demi étoile", "Ether"];
     let inventory = ["Potion", "Potion", "Morceau d'étoile", "Demi étoile", "Ether"];
+    let map = ["[ ]\n [ ]\n [ ]\n [ ]\n [ ]"];
     console.log("Vous entrez dans le donjon...");
-    console.log("Vous êtes dans la salle 1");
+    console.log(`Vous êtes dans la salle ${l}.`);
+    if (l === 1) {
+        map = ["[ ]\n [ ]\n [ ]\n [ ]\n [*]"];
+    }
+    let userchoicem1 = prompt("Voulez vous voir la carte?\n Oui\n Non");
+    if (userchoicem1 === "Oui") {
+        console.log(map);
+    }
     console.log("Il y a des monstres dans cette salle, une porte, et entre les deux un coffre.");
     let userChoicef1 = prompt("Voulez vous attaquer l'ennemi?`\n Oui\n Non");
     if (userChoicef1 === "Oui") {
@@ -16,7 +26,8 @@ export function processing(){
         adventurers[0].HPCurrent = adventurers[0].HPCurrent - 10;
         _fight();
     }
-    console.log("Vous avez vaincu les ennemis de la salle 1!")
+    console.log(`Vous avez vaincu les ennemis de la salle ${l}!`)
+    l = l + 1;
     let userChoicec1 = prompt("Vous approchez du coffre?\n Oui\n Non");
     if (userChoicec1 === "Oui") {
         console.log("Vous vous approchez du coffre!");
@@ -30,9 +41,16 @@ export function processing(){
         }
     
     } else if (userChoicec1 === "Non") {
-        console.log("Vous passez à la salle 2!");
+        console.log(`Vous passez à la salle ${l}!`);
     }
-    console.log("Vous êtes dans la salle 2");
+    console.log(`Vous êtes dans la salle ${l}!`);
+    if (l === 2) {
+        map = ["[ ]\n [ ]\n [ ]\n [*]\n [ ]"];
+    }
+    let userchoicem2 = prompt("Voulez vous voir la carte?\n Oui\n Non");
+    if (userchoicem2 === "Oui") {
+        console.log(map);
+    }
     console.log("Il y a des monstres dans cette salle, une porte.");
     let userChoicef2 = prompt("Voulez vous attaquer l'ennemi?`\n Oui\n Non");
     if (userChoicef2 === "Oui") {
@@ -42,9 +60,17 @@ export function processing(){
         adventurers[0].HPCurrent = adventurers[0].HPCurrent - 10;
         _fight();
     }
-    console.log("Vous avez vaincu les ennemis de la salle 2!");
-    console.log("Vous passez à la salle 3");
-    console.log("Vous êtes dans la salle 3");
+    console.log(`Vous avez vaincu les ennemis de la salle ${l} !`);
+    l = l + 1;
+    console.log(`Vous passez à la salle ${l}!`);
+    console.log(`Vous êtes dans la salle ${l}!`);
+    if (l === 3) {
+        map = ["[ ]\n [ ]\n [*]\n [ ]\n [ ]"];
+    }
+    let userchoicem3 = prompt("Voulez vous voir la carte?\n Oui\n Non");
+    if (userchoicem3 === "Oui") {
+        console.log(map);
+    }
     console.log("Il y a des monstres dans cette salle, une porte.");
     let userChoicef3 = prompt("Voulez vous attaquer l'ennemi?`\n Oui\n Non");
     if (userChoicef3 === "Oui") {
@@ -54,8 +80,17 @@ export function processing(){
         adventurers[0].HPCurrent = adventurers[0].HPCurrent - 10;
         _fight();
     }
-    console.log("Vous avez vaincu les ennemis de la salle 3!");
-    console.log("Vous êtes dans la salle 4");
+    console.log(`Vous avez vaincu les ennemis de la salle ${l}!`);
+    l = l + 1;
+    console.log(`Vous passez à la salle ${l}!`);
+    console.log(`Vous êtes dans la salle ${l}!`);
+    if (l === 4) {
+        map = ["[ ]\n [*]\n [ ]\n [ ]\n [ ]"];
+    }
+    let userchoicem4 = prompt("Voulez vous voir la carte?\n Oui\n Non");
+    if (userchoicem4 === "Oui") {
+        console.log(map);
+    }
     console.log("Il y a des monstres dans cette salle, une porte, et entre les deux un coffre.");
     let userChoicef4 = prompt("Voulez vous attaquer l'ennemi?`\n Oui\n Non");
     if (userChoicef4 === "Oui") {
@@ -65,7 +100,8 @@ export function processing(){
         adventurers[0].HPCurrent = adventurers[0].HPCurrent - 10;
         _fight();
     }
-    console.log("Vous avez vaincu les ennemis de la salle 4!");
+    console.log(`Vous avez vaincu les ennemis de la salle ${l}!`);
+    l = l + 1;
     let userChoicec4 = prompt("Vous approchez du coffre?\n Oui\n Non");
     if (userChoicec4 === "Oui") {
         console.log("Vous vous approchez du coffre!");
@@ -79,9 +115,16 @@ export function processing(){
         }
     
     } else if (userChoicec4 === "Non") {
-        console.log("Vous passez à la salle 5!");
+        console.log(`Vous passez à la salle ${l}!`);
     }
-    console.log("Vous êtes dans la salle 5");
+    console.log(`Vous êtes dans la salle ${l}!`);
+    if (l === 5) {
+        map = ["[*]\n [ ]\n [ ]\n [ ]\n [ ]"];
+    }
+    let userchoicem5 = prompt("Voulez vous voir la carte?\n Oui\n Non");
+    if (userchoicem5 === "Oui") {
+        console.log(map);
+    }
     console.log("Il s'agit de la dernière salle du donjon et dedans il y a ... un dragon!");
     let userChoicef5 = prompt("Voulez vous attaquer l'ennemi?`\n Oui\n Non");
     if (userChoicef5 === "Oui") {
